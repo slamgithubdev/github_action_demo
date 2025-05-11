2e6f8c848c9f6a38d60b2336790d0f3d467a0c63
Slam <s7evenlam@gmail.com>
2025-05-11 18:23:02 +0100

    Test PR for GitHub Actions

--- test_app.py
+++ test_app.py
@@ -1,10 +1,16 @@
 #test
-from app import greet
+from app import greet_user
 
 def test_greet():
     """Testing."""
-    result = greet("Steven")
+    result = greet_user("Steven")
     assert result == "Hello, Steven!"
 
-    result = greet("Blah")
+    result = greet_user("Blah")
     assert result == "Hello, Blah!"
+
+    result = greet_user("New Guy")
+    assert result == "Hello, New Guy!"
+
+
+    
